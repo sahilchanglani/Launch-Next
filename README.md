b06b02a15688e7fa2cee70b500b48d9814a74271
sahilchanglani <sahil.changlani@somaiya.edu>
2021-11-15 16:22:57 +0530

    Basic file setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# Launch-next
+<br/>
+Web app for registering and promoting startups. Companies can register their startup and raise funds. Users can like startups, fund startups and chat with them.
---
+++ server/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://sahilkc:Sahil%40314@launch-next.ey6ok.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Db connceted on ${PORT}`));
+  })
+  .catch((error) => console.log(error.message));
+
