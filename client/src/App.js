9a0a7156e747719c8ae5262164c5074828c408da
kapilwagh <kapil.wagh@somaiya.edu>
2021-11-24 01:39:40 +0530

    added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
 import React from "react";
+import {Container, AppBar, Typography, Grid, Grow, Box, Toolbar, IconButton, Button} from "@material-ui/core";
+import logo from "./images/logo.png";
+import useStyles from "./styles"
 
 const App = () => {
+  const classes = useStyles();
   return (
-    <div>
-      <h1>Launch Next</h1>
-    </div>
-  );
+    <Container maxwidth="lg">
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="fixed">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              <img className={classes.image} src={logo} alt="logo" align="center" height="8%" width="8%" />Launch Next
+            </Typography>
+            <Button variant="contained" size="medium" color="#029C63">Submit Startup</Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </Container>
+  ); 
 };
 
 export default App;
---
+++ client/src/components/form/form.js
@@ -0,0 +1,28 @@
+import TextField from "@material-ui/core/TextField";
+import React, { useState } from "react";
+import { Button, Paper } from "@material-ui/core";
+
+const Form = () => {
+  const [textValue, setTextValue] = useState < string > "";
+
+  const onTextChange = (e) => setTextValue(e.target.value);
+  const handleSubmit = () => console.log(textValue);
+  const handleReset = () => setTextValue("");
+
+  return (
+    <Paper>
+      <h2>Form Demo</h2>
+
+      <TextField
+        onChange={onTextChange}
+        value={textValue}
+        label={"Text Value"} //optional
+      />
+
+      <Button onClick={handleSubmit}>Submit</Button>
+      <Button onClick={handleReset}>Reset</Button>
+    </Paper>
+  );
+};
+
+export default Form;
