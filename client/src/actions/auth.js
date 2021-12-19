44923292071ded8229f50df512f9f9be7c42c06b
kapilwagh <kapil.wagh@somaiya.edu>
2021-12-19 16:56:35 +0530

    updated auth.js

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -5,7 +5,7 @@ export const signin = (formData, navigate) => async (dispatch) => {
     try {
         // log in the user..
 
-        navigate.push('/');
+        navigate('/');
     } catch (error) {
         console.log(error);
     }
@@ -15,7 +15,7 @@ export const signup = (formData, navigate) => async (dispatch) => {
     try {
         // sign up the user..
 
-        navigate.push('/');
+        navigate('/');
     } catch (error) {
         console.log(error);
     }
